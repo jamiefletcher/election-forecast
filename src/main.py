1cd566f79ebf5221cb76d4fa6ae8d97675367228
Jamie Fletcher <68712911+jamiefletcher@users.noreply.github.com>
2024-09-16 15:42:04 -0400

    Add some comments to main.py

--- src/main.py
+++ src/main.py
@@ -6,8 +6,10 @@ from sklearn.svm import SVC
 from process_data import merge_dfs, prepare_census, prepare_elections
 from train import feature_select, model_select
 
-# Filepaths
-CENSUS_2013R = "data/CanCensus2021_2013Ridings/98-401-X2021010_English_CSV_data.csv"
+# 2021 Census - (Old) 2013 Ridings
+census_2013r = "data/CanCensus2021_2013Ridings/98-401-X2021010_English_CSV_data.csv"
+
+# National Election Results - 2015, 2019, 2021
 riding_results = {
     2021: "data/Results/44th_table_tableau12.csv",
     2019: "data/Results/43rd_table_tableau12.csv",
@@ -19,9 +21,7 @@ national_results = {
     2015: "data/Results/42nd_table_tableau09.csv",
 }
 
-# Constants
-TEST_SIZE = 0.2
-
+# Model Types to Compare
 models = {
     "ridge": RidgeClassifier(),
     "logit": LogisticRegression(max_iter=2000),
@@ -30,9 +30,11 @@ models = {
     "mlp": MLPClassifier(hidden_layer_sizes=(50, 20)),
 }
 
+# Test-Train Split
+test_size = 0.2
 
 def main():
-    df_census = prepare_census(CENSUS_2013R)
+    df_census = prepare_census(census_2013r)
     df_elections = prepare_elections(riding_results, national_results)
     dataset = merge_dfs(
         df_census, df_elections, target_class="winner", merge_class="id"
@@ -43,7 +45,7 @@ def main():
     y = dataset["winner"]
 
     X_select = feature_select(X, y)
-    model_select(ids, X_select, y, models, test_size=TEST_SIZE)
+    model_select(ids, X_select, y, models, test_size=test_size)
 
 
 if __name__ == "__main__":
